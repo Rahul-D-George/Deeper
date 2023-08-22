878ebfb66d85cc77bea1e3895dcc493ea9be67ad
Rahul George <rahuld.george@gmail.com>
2023-08-22 10:27:45 +0100

    feat: added code to create training set, with zipped X and Y lists. also added skeleton for model

--- Deeper_v0/training_set_utils.py
+++ Deeper_v0/training_set_utils.py
@@ -1,5 +1,22 @@
-def training_set_create(exclude=None):
-    if exclude is None:
-        #do something
+from PIL import Image
+import pandas as pd
+import os
+
+
+def character_ages():
+    character_df = pd.read_excel("MoreCharacterInfo.xlsx")
+    ages = character_df["Age"].values.tolist()
+    return ages
+
+
+def training_set_create(args=None):
+    if args is None:
+        base_names = os.listdir("img_set")
+        dir_image_names = list(map(lambda x: os.path.join(os.getcwd(), "img_set\\" + x), base_names))
+        image_data = []
+        for image in dir_image_names:
+            image_data.append(Image.open(image))
+        return list(zip(image_data, character_ages()))
     else:
-        # do something else
+        # Currently does nothing
+        return None
