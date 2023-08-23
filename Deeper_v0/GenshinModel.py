e1f8ce661d2405dc3b120a37f9f056160428b815
Rahul George <rahuld.george@gmail.com>
2023-08-23 20:18:19 +0100

    fix: slightly better hyperparameters, leading to relative convergence within 5 epochs

--- Deeper_v0/GenshinModel.py
+++ Deeper_v0/GenshinModel.py
@@ -3,5 +3,5 @@ from model_utils import NeuralNetwork
 
 data = training_set_create()
 
-GenshinNet = NeuralNetwork([len(data[0]), 4, 4, 4, 4, 4, 4, 1], 0.1, data, epochs=10000)
+GenshinNet = NeuralNetwork([len(data[0]), 4, 4, 4, 4, 1], 0.1, data, epochs=30)
 GenshinNet.train()
