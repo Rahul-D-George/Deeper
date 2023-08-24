f78058846ea6928fd950197228b6d35f52d36365
Rahul George <rahuld.george@gmail.com>
2023-08-24 22:59:05 +0100

    fix: fixed up cache format, restructured forward prop for oop-based nn

--- Deeper_v0/model_utils.py
+++ Deeper_v0/model_utils.py
@@ -12,7 +12,7 @@ class NeuralNetwork:
     # n_sizes   =   Size of each layer in neurons, passed as a list. Includes the
     #               number of inputs. E.g.: [3, 4, 3, 2, 1]
     # w, b      =   Arrays of parameters for each layer, i.e: b[3] corresponds to b for layer 3.
-    # cache     =   Used to store Z[l] and A[l] in a (n, 2) array. DOES NOT INCLUDE A0.
+    # caches     =   Used to store A and Z caches as a list of tuples.
     def __init__(self, n_sizes, lr, train_data, g=None, gprime=None, epochs=None):
         assert n_sizes[0] == len(train_data[0])
         assert n_sizes[-1] == 1
@@ -20,45 +20,55 @@ class NeuralNetwork:
 
         # Xavier/Glorot Initialisation
         self.params = {}
-        #self.W = []
         for l in range(1, self.n):
             xgb = sqrt(6) / sqrt(n_sizes[l] + n_sizes[l - 1])
             self.params["W"+str(l)] = np.array([u(-xgb, xgb, n_sizes[l - 1]) for _ in range(n_sizes[l])])
             self.params["b"+str(l)] = np.zeros((n_sizes[l], 1))
-            #self.W.append(np.array([u(-xgb, xgb, n_sizes[l - 1]) for _ in range(n_sizes[l])]))
-
-        #self.b = [np.array([[0] for _ in range(n_sizes[i])]) for i in range(1, self.n)]
         self.lr = lr
         self.X = np.array(train_data[0])
         self.Y = np.array(train_data[1])
-        self.cache = [[np.array([]), np.array([])] for _ in range(self.n)]
-        self.cache[0][1] = self.X
+        self.caches = [[]]
         self.m = len(self.Y)
+        self.final_activation = np.empty(np.shape(self.Y))
         self.cost = 0
-        if g is None:
-            self.g = [np.tanh for _ in range(self.n - 1)]
-            self.g.append(lambda x: x)
-            self.gprime = [self.tanh_derivative for _ in range(self.n - 1)]
-            self.gprime.append(lambda x: 1)
-        else:
-            self.g = g
-            self.gprime = gprime
         if epochs is None:
             self.epochs = 100
         else:
             self.epochs = epochs
+        # if g is None:
+        #     self.g = [np.tanh for _ in range(self.n - 1)]
+        #     self.g.append(lambda x: x)
+        #     self.gprime = [self.tanh_derivative for _ in range(self.n - 1)]
+        #     self.gprime.append(lambda x: 1)
+        # else:
+        #     self.g = g
+        #     self.gprime = gprime
 
     def __mse_cost(self):
-        self.cost = np.sum((self.cache[-1][1] - self.Y) ** 2) / self.m
+        self.cost = np.sum((self.final_activation - self.Y) ** 2) / self.m
+
+    @staticmethod
+    def __forward_prop_calcs(A_prev, W, b, activation):
+        Z = np.dot(W, A_prev) + b
+        return activation(Z), ((A_prev, W, b), Z) # Returns A and Cache
 
     def __forward_prop(self):
+        caches = []
         A = self.X
         for l in range(1, self.n):
             W = self.params["W" + str(l)]
             b = self.params["b" + str(l)]
-            Z = np.dot(W, A) + b
-            A = self.g[l](Z)
-            self.cache[l] = [Z, A]
+            A_prev = A
+            tanh = lambda x: np.tanh(x)
+            A, cache = self.__forward_prop_calcs(A_prev, W, b, tanh)
+            caches.append(cache)
+        Wn = self.params["W" + str(self.n)]
+        bn = self.params["b" + str(self.n)]
+        linear = lambda x : np.identity(x)
+        self.final_activation, finalC = self.__forward_prop_calcs(A, Wn, bn, linear)
+        caches.append(finalC)
+        self.caches = caches
+
 
     def __gradient_descent(self):
         dA = 2 * (self.cache[-1][1] - self.Y)
